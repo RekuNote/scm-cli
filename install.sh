abe0fc29250f2cb36632d81667a88402e235942e
RekuNote <rekunote@gmail.com>
2024-06-12 03:34:40 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check OS
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    OS="linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    OS="macos"
+else
+    echo "Unsupported OS. Please try running the portable version."
+    exit 1
+fi
+
+# Check for Windows
+if [[ "$OS" == "linux" && $(uname -r) == *microsoft* ]]; then
+    echo "Windows is not supported."
+    exit 1
+fi
+
+# Download scm-cli script
+echo "Downloading scm-cli script..."
+curl -o scm-cli https://raw.githubusercontent.com/YourGitHubUsername/YourRepoName/main/scm-cli
+
+# Make the script executable
+chmod +x scm-cli
+
+# Move the script to a directory in PATH
+if [[ "$OS" == "linux" ]]; then
+    sudo mv scm-cli /usr/local/bin/
+elif [[ "$OS" == "macos" ]]; then
+    sudo mv scm-cli /usr/local/bin/
+fi
+
+echo "scm-cli has been installed. You can now run it from anywhere using the command 'scm-cli'."
+
+# Provide a hint to the user to restart their shell if needed
+echo "If you get a 'command not found' error, please restart your terminal or run 'source ~/.bashrc' or 'source ~/.zshrc' (depending on your shell)."
